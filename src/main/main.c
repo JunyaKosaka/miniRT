492fb0e91a3808569db2e95117ebbd8f218a2a93
Junya Kosaka <kosakajunya@gmail.com>
2023-04-30 15:24:07 +0900

    ...

--- src/main/main.c
+++ src/main/main.c
@@ -5,7 +5,8 @@ int main(void)
     void *mlx;
 
     mlx = mlx_init();
+    void    *win;
+    win = mlx_new_window(mlx, 200, 200, " miniRT ");
     (void)mlx;
-    printf("here\n");
     return (0);
 }
