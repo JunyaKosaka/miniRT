63d06af9afeb723bf28e7e9bd5bf399f499a55ad
Junya Kosaka <kosakajunya@gmail.com>
2023-04-30 15:33:09 +0900

    ...

--- src/main/main.c
+++ src/main/main.c
@@ -3,10 +3,8 @@
 int main(void)
 {
     t_miniRT    miniRT;
-    void *mlx;
 
     miniRT.mlx = mlx_init();
-    void    *win;
     miniRT.win = mlx_new_window(miniRT.mlx, 200, 200, " miniRT ");
     miniRT.img.img = mlx_new_image(miniRT.mlx, WIN_SIZE, WIN_SIZE);
 	miniRT.img.addr = mlx_get_data_addr(miniRT.img.img, &miniRT.img.bits_per_pixel, \
