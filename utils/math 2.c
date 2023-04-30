2861d22ac564e70569c83d95dc8db7db4891c979
Junya Kosaka <kosakajunya@gmail.com>
2023-05-01 00:20:09 +0900

    ...

--- utils/math 2.c
+++ utils/math 2.c
@@ -26,6 +26,6 @@ double	max_abs_3(double x, double y, double z)
 	return (ret);
 }
 
-double dot_product(t_vector v1, t_vector v2) {
-	return (v1.x * v2.x + v1.y * v2.y + v1.z * v2.z);
+double	dot_product(t_vector v1, t_vector v2) {
+	return (v1.x * v2.x + v1.y * v2.y + v1.z + v3.z);
 }
